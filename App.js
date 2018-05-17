affba6e24a20fc1f5d865de7f8035bbbf2c1d16e
cshashwat <Shashwat.Chaturvedi.18@dartmouth.edu>
2018-05-17 01:38:40 -0400

    Added all code, app now works

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MainTabBar from './navigation/main_tab_bar';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+// disable really annoying in app warnings
+console.disableYellowBox = true;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = (props) => {
+  return <MainTabBar />;
+};
+
+
+export default App;
